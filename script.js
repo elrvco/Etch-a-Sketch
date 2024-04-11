17e2ece75b0b939fc9a5f5aff9ce95594b98efc5
elruco <ing.garcia1995@gmail.com>
2024-04-11 11:44:38 -0600

    the grid squares change color whe user hover, user input is required to create a grid
    only works for 4x4 grids

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const container = document.querySelector('#container');
-for (let i=0; i < 16; i++){
+
+const btn= document.querySelector('button');
+btn.addEventListener('click',()=> {
+    blockNumber = prompt('Enter the number of squares per side for your new'+
+    'grid (ie: if you enter 10 => a 10x10 grid will be created');
+    for (let i=0; i < blockNumber**2; i++){
         let div = document.createElement('div');
         container.appendChild(div);
         div.setAttribute('class','boxes');
-}
+    }
+    const divList= document.querySelectorAll('.boxes');
+    divList.forEach((div)=>{
+    div.addEventListener('mouseenter',()=> div.style.backgroundColor= 'blue')
+    })
+    
+})
+
+
+
+
