489dc48dad8bbb14452a7e5b1a57c5185fd1e4bc
elruco <ing.garcia1995@gmail.com>
2024-04-12 11:45:55 -0600

    Improved stylesheet and fixed grid limitations
    Created a js module to randomize the square colors
    Limited grid size to: <= 100x100 grid

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 const container = document.querySelector('#container');
-
 const btn= document.querySelector('button');
 btn.addEventListener('click',()=> {
+    if (container.childNodes.length > 0){
+        const childList= Array.from(container.childNodes);
+        childList.forEach((child)=> container.removeChild(child))
+    }
     blockNumber = prompt('Enter the number of squares per side for your new'+
     'grid (ie: if you enter 10 => a 10x10 grid will be created');
-    for (let i=0; i < blockNumber**2; i++){
-        let div = document.createElement('div');
-        container.appendChild(div);
-        div.setAttribute('class','boxes');
-    }
-    const divList= document.querySelectorAll('.boxes');
-    divList.forEach((div)=>{
-    div.addEventListener('mouseenter',()=> div.style.backgroundColor= 'blue')
-    })
-    
+        if (blockNumber <= 100){
+            for (let i=0; i < blockNumber**2; i++){
+                let div = document.createElement('div');
+                container.appendChild(div);
+                div.setAttribute('class','boxes');
+            }
+            const divList= document.querySelectorAll('.boxes');
+            divList.forEach((div)=>{
+            let squareSize = 100/blockNumber + '%';
+            div.style.flexBasis = squareSize;
+            div.addEventListener('mouseenter',()=> div.style.backgroundColor= getRandomColor())
+            })
+        }else{
+            alert('please input a valid value. No more than 100 is accepted')
+        }
 })
 
-
+function getRandomColor(){
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    return 'rgb'+'('+r+','+g+','+ b+')'
+}
 
 
